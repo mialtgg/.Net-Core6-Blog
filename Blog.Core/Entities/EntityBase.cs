d2e6ce789c2d2e237d4e4bf68e1e31d7b8ea0417
Minealtug <200316021@ogr.cbu.edu.tr>
2023-06-14 15:14:49 +0300

    Entity Base

---
+++ Blog.Core/Entities/EntityBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Entities
+{
+   public  abstract class EntityBase :IEntityBase
+    {
+        public virtual Guid Id { get; set; } = Guid.NewGuid();
+        public virtual string? DelatedBy { get; set; }
+        public virtual   string?  CreatedBy { get; set; }
+        public virtual  string? ModifiedBy { get; set; }
+        public virtual DateTime? CreatedDate { get; set; } = DateTime.Now;
+        public virtual DateTime? DelatedDate  { get; set; }
+        public virtual DateTime? ModifiedDate { get; set; }
+
+        public virtual  bool IsDelate { get; set; }
+    }
+}
