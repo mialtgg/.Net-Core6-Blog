3c8d42e767d626c1eb66e9f579b32557ad082b9d
Minealtug <200316021@ogr.cbu.edu.tr>
2023-07-12 20:30:13 +0300

    .

---
+++ Blog.Service/Services/Concrete/ArticleService.cs
@@ -0,0 +1,22 @@
+﻿using Blog.dATA.UnitOfWorks;
+using Blog.Entity.Entities;
+using Blog.Service.Services.Abstractions;
+using System;
+
+
+namespace Blog.Service.Services.Concrete
+{
+    public class ArticleService : IArticleService
+    {
+        private readonly IunitOfWork unitofWork;
+
+        public ArticleService(IunitOfWork unitofWork)
+        {
+            this.unitofWork = unitofWork;
+        }
+        public async Task<List<Article>> GetAllArticlesAsync()
+        {
+            return await unitofWork.GetRepository<Article>().GetAllAsync();
+        }
+    }
+}
